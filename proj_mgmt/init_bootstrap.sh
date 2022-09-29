56807dd85cb4932324a4258dc4b982b284ca34ba
Robert McLay <mclay@zaniahgroup.com>
2022-09-29 12:03:18 -0500

    updates: cannot run root_bootstrap.sh from mclay account

--- proj_mgmt/init_bootstrap.sh
+++ proj_mgmt/init_bootstrap.sh
@@ -67,15 +67,9 @@ cleanup ()
   rm -f ~mclay/pkg.tar.gz
 }
 
-run_root_bootstrap ()
-{
-  ssh -YA root@localhost ./root_bootstrap.sh
-}
-
 cmdA=("install_ssh_keys"
       "minimal_pkg_install"
       "cleanup"
-      "run_root_bootstrap"
       )
 
 runMe "${cmdA[@]}"
